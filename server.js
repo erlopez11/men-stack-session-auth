7c7925306870a200454e372c86b98ff73f33b8ea
erlopez11 <lopezelainy@gmail.com>
2025-03-10 12:47:21 -0500

    wip: add server boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
+//Dependecies
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 
+
+//Initialize Express
+const app = express();
+
+//Configure Settings
+dotenv.config();
+const port = process.env.PORT || '3000';
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+//Mount Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+
+//Mount Routes
+
+
+
+
+//Tell App to Listen for HTTP requests
+app.listen(port, () => {
+    console.log(`The express app is ready on the port ${port}!`);
+});
