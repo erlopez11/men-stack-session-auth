dffcb0137c88dfe116456e0b73d8d027335259d6
erlopez11 <lopezelainy@gmail.com>
2025-03-11 16:45:22 -0500

    add sign out router that destroys session

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,4 +65,9 @@ router.post('/sign-in', async (req, res) => {
     res.redirect('/');
 })
 
+router.get('/sign-out', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
 module.exports = router;
